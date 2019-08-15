f8ed29e3bad506b25a6477f6475d39b67bbed2cd
Alex Smetukh <alex.smetukh@gmail.com>
2019-08-15 20:26:23 +0300

    add routes, controllers

---
+++ server/routes/api.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+	  router = express.Router(),
+	//   authController = require('../controllers/auth.controller'),
+	  appsController = require('../controllers/apps.controller');
+
+router.get('/ios-apps', appsController.getiOSApps);
+router.get('/android-apps', appsController.getAndroidApps);
+
+// router.post('/register', authController.register);
+// router.post('/login', authController.login);
+
+module.exports = router;
+
--- server/server/app.js
+++ server/server/app.js
@@ -3,6 +3,11 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const api = require('../routes/api');
+const path = require('path');
+const appRoot = require('app-root-path');
+const config = require('../config');
 
 const app = express();
 const PORT = 3005;
@@ -15,6 +20,27 @@ app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+// qooco
+app.use(bodyParser.json());
+
+// Set our api routes
+app.use('/api', api);
+
+
+app.use(express.static(path.join(appRoot.path, 'dist')));
+
+if (!config.ios_apps_dir.startsWith(appRoot.path)) {
+	app.use("/data/ios/files/", express.static(config.ios_apps_dir));
+}
+
+if (!config.android_apps_dir.startsWith(appRoot.path)) {
+	app.use("/data/android/files/", express.static(config.android_apps_dir));
+}
+
+app.use("/data/ios/", express.static(path.resolve(appRoot.path, "./data/ios/")));
+app.use("/data/android/", express.static(path.resolve(appRoot.path, "./data/android/")));
+//
 
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error: ${err}`));
